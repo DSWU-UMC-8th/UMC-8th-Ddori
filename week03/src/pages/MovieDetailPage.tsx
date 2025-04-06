5dc79d0d15c3ca0f2235ac4675e8ea7591881dad
heeyun817 <vmffpfm79@naver.com>
2025-04-06 16:33:45 +0900

    ✨ Feat: 로딩 에러 처리 및 여러개의 페이지 라우팅 처리 #5
    - 이전 미션 1에서 진행한 프로젝트를 확장하여, `react-router-dom`을 활용한 다중 페이지 라우팅을 구현
    - '로딩 처리'와 '에러 처리'를 추가하고, '페이지네이션'을 적용

---
+++ week03/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom"
+
+const MovieDetailPage = () => {
+  const params = useParams();
+  return (
+    <div>MovieDetailPage{params.movieId}</div>
+  )
+}
+
+export default MovieDetailPage
