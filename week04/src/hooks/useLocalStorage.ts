f0cb6d0b7f75acc3aa3a540413ef45c39f874fde
heeyun817 <vmffpfm79@naver.com>
2025-05-04 15:23:31 +0900

    🐛 Fix: 로그아웃 오류 수정 #9
    useLocalStorage.ts 수정

--- week04/src/hooks/useLocalStorage.ts
+++ week04/src/hooks/useLocalStorage.ts
@@ -1,28 +1,27 @@
 export const useLocalStorage = (key: string) => {
-  const setItem = (value: unknown) => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.log(error);
-    }
+  const setItem = (value: string) => {
+      try {
+          window.localStorage.setItem(key, value);
+      } catch (error) {
+          console.log(error);
+      }
   };
 
   const getItem = () => {
-    try {
-      const item = window.localStorage.getItem(key);
-
-      return item ? JSON.parse(item) : null;
-    } catch (error) {
-      console.log(error);
-    }
+      try {
+          return window.localStorage.getItem(key);
+      } catch (e) {
+          console.log(e);
+          return null;
+      }
   };
 
   const removeItem = () => {
-    try {
-      window.localStorage.removeItem(key);
-    } catch (error) {
-      console.log(error);
-    }
+      try {
+          window.localStorage.removeItem(key);
+      } catch (error) {
+          console.log(error);
+      }
   };
 
   return { setItem, getItem, removeItem };
